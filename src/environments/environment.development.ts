1ae63fbff63c192059683b0251cffa6fe744f025
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2025-05-20 21:30:56 +0100

    feat (environment.ts, environment.development.ts): implements environment configurations
    Adds environment configuration files to manage different
    settings for development and production environments.
    Defines base URL and endpoints for various services.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,30 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    baseUrl: 'http://localhost:8080',
+    ENDPOINT: {
+        // Auth Service
+        register: () => `${environment.baseUrl}/api/auth/register`,
+        login: () => `${environment.baseUrl}/api/auth/login`,
+        
+        // Civilization Management
+        civilizations: () => `${environment.baseUrl}/api/civilizations`,
+        civilizationById: (id: string) => `${environment.baseUrl}/api/civilizations/${id}`,
+        
+        
+        // Events Management
+        events: () => `${environment.baseUrl}/api/events`,
+        eventById: (id: string) => `${environment.baseUrl}/api/events/${id}`,
+        eventsByCivilization: (civilizationId: string) => 
+        `${environment.baseUrl}/api/events/civilization/${civilizationId}`,
+        
+        // Comments Management
+        comments: () => `${environment.baseUrl}/api/comments`,
+        commentsByEvent: (eventId: string) => 
+        `${environment.baseUrl}/api/comments/event/${eventId}`,
+        
+        // Media Management
+        media: () => `${environment.baseUrl}/api/media`,
+        mediaByEvent: (eventId: string) => 
+        `${environment.baseUrl}/api/media/event/${eventId}`,
+    }
+};
