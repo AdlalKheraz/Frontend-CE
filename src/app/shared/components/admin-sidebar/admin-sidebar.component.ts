29c2f85184da57dcfcbcd217a5bdd555b21aefa5
Yaici-Yacine <yacine.yaici@se.univ-bejaia.dz>
2025-05-24 23:02:24 +0100

    Refactor (admin-sidebar.component.html, admin-sidebar.component.ts): improves user profile display in sidebar
    Refactors the admin sidebar to improve the display of the user profile section. This includes moving the user profile section to the top of the sidebar, displaying the user's avatar, and optimizing the overall layout for better readability.

--- src/app/shared/components/admin-sidebar/admin-sidebar.component.ts
+++ src/app/shared/components/admin-sidebar/admin-sidebar.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit, inject } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { User } from '@app/core/auth/auth.service';
 import { AuthService } from '@core/auth/auth.service';
+import { generateAvatar } from '@core/avatar/avatar.lib';
 
 @Component({
   selector: 'app-admin-sidebar',
@@ -18,6 +19,7 @@ export class AdminSidebarComponent implements OnInit {
   private authService = inject(AuthService);
   
   currentUser: User | null = null;
+  avatar=generateAvatar(this.currentUser?.firstName || 'default');
 
   ngOnInit(): void {
     this.loadCurrentUser();
@@ -26,6 +28,8 @@ export class AdminSidebarComponent implements OnInit {
   loadCurrentUser(): void {
     this.authService.authState$.subscribe(state => {
       this.currentUser = state.user;
+      this.avatar=generateAvatar(state.user?.firstName || 'default');
+
     });
   }
 
